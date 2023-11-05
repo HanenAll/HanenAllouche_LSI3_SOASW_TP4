9d91e623a0003df716640fc6c4c2316f2658092c
Hanen Allouche <106692503+HanenAll@users.noreply.github.com>
2023-11-05 18:43:41 +0100

    Update ServerJWS.java

--- src/main/java/server/ServerJWS.java
+++ src/main/java/server/ServerJWS.java
@@ -6,7 +6,8 @@ import service.CalculatriceWS;
 public class ServerJWS {
     public static void main(String[] args) {
         String url ="http://localhost:8084/";
-        Endpoint.publish(url,new CalculatriceWS());
+        Endpoint.publish(url,new CalculatriceWS()); /* Endpoint.publish(..) me permet de faire le deployement
+         de web service */
         System.out.println(url);
     }
 }
